e191ce8d00c4052bb8ad7d645c672ac77cbe5b34
Anuj Bhagat <157007144+anuj1611@users.noreply.github.com>
2025-05-24 12:45:31 +0530

    Time: 2 ms (82.73%) | Memory: 46.3 MB (19.66%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,14 +1,17 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int left = 0;
+        int left = 0;   // \U0001f446 Pointer for the next non-zero position
+        int right = 0;  // \U0001f446 Pointer to scan the array
 
-        for (int right = 0; right < nums.length; right++) {
-            if (nums[right] != 0) {
-                int temp = nums[right];
-                nums[right] = nums[left];
-                nums[left] = temp;
-                left++;
+        while (right < nums.length) {  // \U0001f504 Loop through the array
+            if (nums[right] != 0) {  // \U0001f575️‍♀️ Found a non-zero number!
+                // \U0001f504 Swap nums[left] and nums[right]
+                int temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
+                left++;  // ➡️ Move left to the next position
             }
-        }        
+            right++; // ➡️ Always move right to the next position
+        }
     }
 }
