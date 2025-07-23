bb235286e287a5613d92568fc9d8153f3f56a843
Anuj Bhagat <157007144+anuj1611@users.noreply.github.com>
2025-07-23 09:25:00 +0530

    Time: 0 ms (100.00%) | Memory: 44.9 MB (83.89%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -10,16 +10,17 @@ class Solution {
     }
 
     public void fill(int[][] image, int sr, int sc, int color, int cur) {
+        //handling edge case, index out of bound
         if (sr < 0 || sr >= image.length || sc < 0 || sc >= image[0].length) {
             return;
         }
-
+        // if the current pixel is not equal to the next pixel 
         if (image[sr][sc] != cur) {
             return;
         }
-
+        //change the color to desired color
         image[sr][sc] = color;
-
+        // recursion, do the same thing for the element to the N,W,S,E
         fill(image, sr - 1, sc, color, cur);
         fill(image, sr + 1, sc, color, cur);
         fill(image, sr, sc - 1, color, cur);
