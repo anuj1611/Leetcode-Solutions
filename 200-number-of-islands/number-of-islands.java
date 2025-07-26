6469f07f93f8d0a857bff3fe01d163fb90d8159f
Anuj Bhagat <157007144+anuj1611@users.noreply.github.com>
2025-07-26 23:22:25 +0530

    Time: 3 ms (82.95%) | Memory: 49.5 MB (71.95%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,33 +1,27 @@
 class Solution {
-
-    public static void makeZero( int i, int j, int r, int c, char[][] grid){
-        if (i<0 || j<0 || i>=r || j>=c || grid[i][j] == '0'){
+    public static void makeZero(int i,int j,int r,int c,char grid[][]){
+        if(i<0||j<0||i>=r||j>=c||grid[i][j]=='0'){
             return;
         }
-        //marking current cell as visited by setting it to '0'
         grid[i][j]='0';
-
         makeZero(i+1,j,r,c,grid);
         makeZero(i-1,j,r,c,grid);
         makeZero(i,j+1,r,c,grid);
         makeZero(i,j-1,r,c,grid);
+
     }
     public int numIslands(char[][] grid) {
-        int island=0;
+        int island=0; 
         int r=grid.length;
         int c=grid[0].length;
-
-        //for loop to traverse the grid
-        for(int i=0; i<r;i++){
-            for(int j=0;j<c;j++){
+        for (int i=0;i<r;i++){
+            for (int j=0;j<c;j++){
                 if(grid[i][j]=='1'){
-                    island +=1;
+                    island+=1;
                     makeZero(i,j,r,c,grid);
                 }
             }
         }
-        return island; //returning the total number of islands
+        return island;
     }
 }
-
-
