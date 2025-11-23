b427b18c3a4b082db92dd5696bf4f439dfbcfc38
Anuj Bhagat <157007144+anuj1611@users.noreply.github.com>
2025-11-23 12:50:38 +0530

    Time: 2 ms (83.22%) | Memory: 47.7 MB (22.96%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -2,6 +2,8 @@ class Solution {
     public void moveZeroes(int[] nums) {
         int left = 0;
 
+//we use two pointers left and i. left is slow pointer and i is faast pointer. 
+// both start at first element of array. we use loop of i, i keepps going until
         for (int i=0; i <nums.length; i++){
             if (nums[i]!=0){
                 int temp = nums[left];
