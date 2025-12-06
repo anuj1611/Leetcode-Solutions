973c4a9845c55ce8b2c092814cf9a721946be15a
Anuj Bhagat <157007144+anuj1611@users.noreply.github.com>
2025-12-06 11:37:02 +0530

    Time: 1 ms (9.66%) | Memory: 47.6 MB (54.02%) - LeetSync

---
+++ 792-binary-search/binary-search.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int foundNum = -1;
+
+        for (int i=0; i <nums.length; i++){
+            if( nums[i] == target){
+                foundNum = i;
+                break;
+            }
+        }
+        return foundNum;
+        
+    }
+}
